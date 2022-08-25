50e884a2597558bab03139b733ca1d9417f04c9f
Ruslana Nikolaev <101888519+romashka79@users.noreply.github.com>
2022-08-24 22:05:11 -0500

    Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -13,3 +13,18 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # SUMMARY STATISTICS
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
+
+# DELIVERABLE 2
+
+# IMPORT AND READ CSV
+Suspension_Coil <- read.csv(file='/Users/kukla/Desktop/Class/MechaCar_Statistical_Analysis/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+# STATS SUMMARY
+# using summerize()
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+View(total_summary)
+
+#STATS SUMMARY
+# using group_by()
+lot_summary <- Suspention_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep')
+View(lot_summary)
